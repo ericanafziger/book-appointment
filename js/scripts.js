29a84523666c83f8f2ab548ec775f3320dc1d85e
Erica Nafziger and Luke Philips <erica@>
2016-10-19 15:22:28 -0700

    add dropdown, populate conf letter, activate all JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-
+console.log("page ready");
   $(function(){
     $("#datepicker").datepicker();
+  });
+console.log("date function done");
+// debugger;
+  $("#booking form").submit(function(event) {
+
+console.log("function start");
+    var yourNameInput = $("input#yourName").val();
+    var datepickerInput = $("input#datepicker").val();
+    var lengthInput = $("#length").val();
+    var commentsInput = $("input#comments").val();
+console.log("variables set");
+    $(".yourName").text(yourNameInput);
+    $(".datepicker").text(datepickerInput);
+    $(".length").text(lengthInput);
+    $(".comments").text(commentsInput);
+
+    $("#booking").hide();
+    $("#confirmation").show();
+    event.preventDefault();
   });
 
 });
